b05042735a50dc84bab5559fe19b71d134150adc
steichten <steichten@gmail.com>
2014-06-04 15:14:03 +1000

    more anno

--- dmr_merge.r
+++ dmr_merge.r
@@ -4,9 +4,15 @@ args=commandArgs(trailingOnly=T)
 print(args)
 
 ############
-#grab all the individual bedtoops results
+# quick script to grab bedtools results, sum across C's in DMR window, and make final table
+############
+
+
+#grab all the individual bedtools results (*.dmr)
+
 filelist=dir(pattern="*.dmr$")
 
+#read them in and add a row with the samplename. Rbind them all together
 tes=read.delim(filelist[1],head=F)
 group=rep(strsplit(filelist[1],"_")[[1]][1],nrow(tes))
 tes=cbind(tes,group)
@@ -25,9 +31,12 @@ colnames(t2)[4:ncol(t2)]=paste(names(t2[4:ncol(t2)]),"_met",sep='')
 t3=dcast(tes, V1 + V2 + V3 ~ group,value.var='V6')
 colnames(t3)[4:ncol(t3)]=paste(names(t3[4:ncol(t3)]),"_unmet",sep='')
 
+#make a table of it all
 tout=cbind(t1,t2[,4:9],t3[,4:9])
 
 #write it out, and write out a version with only rows with data for all samples
 write.table(tout,'all_dmr_metvalues.txt',sep='\t',row.names=F,quote=F)
 toutnarm=na.omit(tout)
 write.table(toutnarm,'all_dmr_metvalues_noNA.txt',sep='\t',row.names=F,quote=F)
+
+#
