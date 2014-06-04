118a8c136d2a1d69b0cea230723620d2c83e95ac
steichten <steichten@gmail.com>
2014-06-04 15:09:35 +1000

    more annotations

--- methylkit_edmr_pipeline_v0.2.r
+++ methylkit_edmr_pipeline_v0.2.r
@@ -3,7 +3,7 @@ library(fields)
 args=commandArgs(trailingOnly=T)
 print(args)
 
-#prerecs
+#LIBRARIES REQUIRED
 library(edmr)
 library(methylKit)
 library(GenomicRanges)
@@ -15,56 +15,54 @@ library(reshape2)
 #EDIT AS NEEDED###########################
 context=args[1]
 min.cov=args[2]
-assembly='Bd21Control'
+assembly='Bd21Control' #currently fixed, may change at a later time
 ##########################################
+
 #list of all sam files created by bismark (sorted!)
 a=dir(pattern="*.sam")
 
 biglist=as.list(a)
 
-#make one gigantic list of all the sam read-ins
+#make one gigantic list of all the sam read-ins; this takes a while & will hit memory issues eventually
 for(i in 1:length(a)){
 biglist[[i]]=read.bismark(a[i],sample.id=strsplit(a[i],'_')[[1]][1],assembly=assembly,save.context=context,read.context=context,treatment=c(0),mincov=min.cov)
 }
 
 aa=combn(a,2) #combinations of filenames
-bb=combn(1:length(biglist),2)
+bb=combn(1:length(biglist),2) #combinations of elements in the biglist
 
-#make methylRawList sets
+#loop through all pairwise combinations#########################
 for(i in 1:(length(aa)/2)){
-#
-myobj=new("methylRawList",list(biglist[[bb[1,i]]],biglist[[bb[2,i]]]),treatment=c(0,1))
+
+	myobj=new("methylRawList",list(biglist[[bb[1,i]]],biglist[[bb[2,i]]]),treatment=c(0,1))
 
 #only keep cytosines in which all samples meet the coverage requirement
-meth=unite(myobj)
+	meth=unite(myobj)
 
-contrast=paste(strsplit(aa[1,i],'_')[[1]][1],strsplit(aa[2,i],'_')[[1]][1],sep='v')
-# cluster all samples using correlation distance and return a tree object for plclust
-#hc = clusterSamples(meth, dist="correlation", method="ward", plot=T)
+#define a string indicating the performed contrast
+	contrast=paste(strsplit(aa[1,i],'_')[[1]][1],strsplit(aa[2,i],'_')[[1]][1],sep='v')
 
-# principal component anlaysis of all samples.
-#PCASamples(meth)
 
 # calculate differential methylation p-values and q-values
-myDiff=calculateDiffMeth(meth)
+	myDiff=calculateDiffMeth(meth)
 
-write.table(getData(myDiff),paste("1_MethylKit_results/",contrast,"_mincov_",min.cov,"_",context,"_",Sys.Date(),".bed",sep=''),sep='\t',row.names=F,quote=F,col.names=F)
-# get differentially methylated regions with 25% difference and qvalue<0.01
-#myDiff25p=get.methylDiff(myDiff,difference=25,qvalue=0.01)
+	write.table(getData(myDiff),paste("1_MethylKit_results/",contrast,"_mincov_",min.cov,"_",context,"_",Sys.Date(),".bed",sep=''),sep='\t',row.names=F,quote=F,col.names=F)
 
+#this list of differential methylation calls (sig or otherwise) is passed to eDMR
 
-#DMRs with eDMR
+#DMRs with eDMR#########################
 
 # fitting the bimodal normal distribution to CpGs distribution
-myMixmdl=myDiff.to.mixmdl(myDiff,plot=F,main=contrast)
+	#myMixmdl=myDiff.to.mixmdl(myDiff,plot=F,main=contrast)
 
 # plot cost function and the determined distance cutoff
 #plotCost(myMixmdl, main="cost function")
 
 # calculate all DMRs candidate
-mydmr=edmr(myDiff, mode=1, ACF=TRUE)
+	mydmr=edmr(myDiff, mode=1, ACF=TRUE)
 
-dmr.output <- data.frame(seqnames=seqnames(mydmr),
+#go through and make a standard table to write out as a bed file@######
+	dmr.output <- data.frame(seqnames=seqnames(mydmr),
   starts=start(mydmr)-1, #coordinate 0-based like bed file
   ends=end(mydmr),
   names=c(rep(contrast,length(mydmr))),
@@ -76,14 +74,13 @@ dmr.output <- data.frame(seqnames=seqnames(mydmr),
   dmr.pvalue=elementMetadata(mydmr)$DMR.pvalue,
   dmr.qvalue=elementMetadata(mydmr)$DMR.qvalue)
   
-write.table(dmr.output,paste("2_eDMR/",contrast,"_mincov_",min.cov,'_',context,'_',Sys.Date(),".bed",sep=''),sep='\t',row.names=F,quote=F,col.names=F)
+	write.table(dmr.output,paste("2_eDMR/",contrast,"_mincov_",min.cov,'_',context,'_',Sys.Date(),".bed",sep=''),sep='\t',row.names=F,quote=F,col.names=F)
 
 
+# further filtering the DMRs#######
+	mysigdmr=filter.dmr(mydmr)
 
-# further filtering the DMRs
-mysigdmr=filter.dmr(mydmr)
-
-dmr.output <- data.frame(seqnames=seqnames(mysigdmr),
+	dmr.output <- data.frame(seqnames=seqnames(mysigdmr),
   starts=start(mysigdmr)-1,
   ends=end(mysigdmr),
   names=c(rep(contrast,length(mysigdmr))),
@@ -95,7 +92,7 @@ dmr.output <- data.frame(seqnames=seqnames(mysigdmr),
   dmr.pvalue=elementMetadata(mysigdmr)$DMR.pvalue,
   dmr.qvalue=elementMetadata(mysigdmr)$DMR.qvalue)
   
-write.table(dmr.output,paste("3_eDMR_filtered/",contrast,"_mincov_",min.cov,'_',context,"_filtered_",Sys.Date(),".bed",sep=''),sep='\t',row.names=F,quote=F,col.names=F)
+	write.table(dmr.output,paste("3_eDMR_filtered/",contrast,"_mincov_",min.cov,'_',context,"_filtered_",Sys.Date(),".bed",sep=''),sep='\t',row.names=F,quote=F,col.names=F)
 
 }
 
